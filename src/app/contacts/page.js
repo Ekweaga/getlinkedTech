2746fc0075209e7e16074d3c81eeb318bf8e1739
Ekweaga <hikay133@gmail.com>
2023-09-26 16:42:03 +0100

    contacting us

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -68,6 +68,7 @@ setSuccess(true)
                     setPhone('')
                     setMail('')
                     setMessage('')
+                    toast('Thanks for contacting us',{autoClose:1000,type:'success',position:'top-right'})
                 }).catch((err)=>{
                     if(err.response.data.email){
                         toast('Email already exist',{autoClose:1000,type:'error',position:'top-right'})
